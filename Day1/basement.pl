ccf0edca33441a29678a6940fba581030c30414b
Abigail <abigail@abigail.be>
2015-12-07 21:25:14 +0100

    Solve both parts

--- Day1/basement.pl
+++ Day1/basement.pl
@@ -5,13 +5,29 @@ use 5.010;
 use strict;
 use warnings;
 no  warnings 'syntax';
+use autodie;
 
+my $input = "input";
+
+open my $fh, "<", $input;
 local $/;          # Slurp mode.
-$_ = <>;           # Read entire input.
-my $floor = 0;     # Santa starts here.
-$floor += y/(/(/;  # Number of floors up.
-$floor -= y/)/)/;  # Number of floors down.
+$_ = <$fh>;        # Read entire input.
 
-say "Final floor: $floor";
+my $floor = 0;
+my $index = 0;
+my $basement_entered;
 
-__END__
+while (/(.)/g) {
+    my $char = $1;
+    next unless $char =~ /[()]/;
+    $index ++;
+    if ($char eq '(') {$floor ++;}
+    if ($char eq ')') {$floor --;}
+    if (!$basement_entered && $floor < 0) {
+        $basement_entered = $index;
+    }
+}
+
+
+say "Solution to part1: $floor";
+say "Solution to part2: $basement_entered";
